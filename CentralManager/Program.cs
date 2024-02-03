9636e823e5d6af4d649a83824905483b3636bf4b
Cristian I. Vargas <civargas34@gmail.com>
2024-02-02 21:26:14 -0400

    Add project files.

---
+++ CentralManager/Program.cs
@@ -0,0 +1,33 @@
+﻿using StackExchange.Redis;
+
+class Program
+{
+    private static List<double> temperatures = [];
+    static void Main()
+    {
+        Console.Write("Ingrese el nombre del equipo que desea monitorear: ");
+        string deviceName = Console.ReadLine();
+
+        using var connection = ConnectionMultiplexer.Connect("localhost");
+        var subscriber = connection.GetSubscriber();
+        subscriber.Subscribe($"ch:{deviceName}", (RedisChannel, value) =>
+        {
+            double temperatura = double.Parse(value);
+            Console.WriteLine($"Temperatura recibida para {deviceName}: {temperatura}");
+
+            temperatures.Add(temperatura);
+            ShowAnalytics();
+        });
+
+        Console.WriteLine($"Sistema central esperando lecturas de temperatura para {deviceName}...");
+        Console.ReadLine();
+    }
+
+    private static void ShowAnalytics()
+    {
+        Console.WriteLine($"Datos de Temperatura:");
+        Console.WriteLine($"Temperatura Minima: {temperatures.Min()}");
+        Console.WriteLine($"Temperatura Maxima: {temperatures.Max()}");
+        Console.WriteLine($"Temperatura Promedio: {Math.Round(temperatures.Average(), 2)}");
+    }
+}
---
+++ SensorEmulator/Program.cs
@@ -0,0 +1,43 @@
+﻿using StackExchange.Redis;
+
+class Program
+{
+    private const string RedisConnectionString = "localhost:6379";
+    private static ConnectionMultiplexer connection = ConnectionMultiplexer.Connect(RedisConnectionString);
+    static void Main()
+    {
+        List<string> devices = [];
+        Console.WriteLine("Registro de Equipos (Ingrese 'exit' para finalizar el registro):");
+
+        while (true)
+        {
+            Console.Write("Ingrese el nombre del equipo (o 'exit' para salir): ");
+            string newDevice = Console.ReadLine();
+
+            if (string.IsNullOrEmpty(newDevice)) break;
+            if (newDevice == "exit") break;
+
+            devices.Add(newDevice);
+        }
+
+        if (devices.Count > 0)
+        {
+            var random = new Random();
+            var publisher = connection.GetSubscriber();
+            while (true)
+            {
+                foreach(var device in devices)
+                {
+                    double deviceTemp = random.Next(40, 70) + random.NextDouble();
+                    deviceTemp = Math.Round(deviceTemp, 2);
+                    string channelName = $"ch:{device}";
+
+                    publisher.PublishAsync(channelName, deviceTemp.ToString(), CommandFlags.FireAndForget);
+                    Console.WriteLine($"Lectura de temperatura para {device}: {deviceTemp}");
+                    Thread.Sleep(1000);
+                }
+            }
+        }
+        return;      
+    }
+}
